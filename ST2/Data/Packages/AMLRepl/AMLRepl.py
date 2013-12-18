eb8d5a6bb4e4bcc44b3430ec57051dbe1b4f6607
Simen Haugerud Granlund <simen@hgranlund.com>
2013-12-18 20:39:22 +0100

    added pep8 foratting to amlrepl.py

--- ST2/Data/Packages/AMLRepl/AMLRepl.py
+++ ST2/Data/Packages/AMLRepl/AMLRepl.py
@@ -1,7 +1,6 @@
 import sublime
 import sublime_plugin
 import subprocess
-import sys
 import time
 import webbrowser
 from threading import Thread
@@ -14,6 +13,7 @@ aml_batch_file = ""
 repl_process = None
 output_lines = []
 output_view = None
+
 
 class AmlReplCommand(sublime_plugin.TextCommand):
 
@@ -35,12 +35,14 @@ class AmlReplCommand(sublime_plugin.TextCommand):
         aml_batch_file = settings.get("aml_batch_file")
 
         global repl_process
-        repl_process = subprocess.Popen(aml_batch_file, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, cwd=aml_start_path)
+        repl_process = subprocess.Popen(
+            aml_batch_file, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, cwd=aml_start_path)
 
         global output_view
         output_view = self.view
 
-        stdout_thread = Thread(target=self.PrintStdout, args=[edit, repl_process])
+        stdout_thread = Thread(
+            target=self.PrintStdout, args=[edit, repl_process])
         stdout_thread.setDaemon(True)
         stdout_thread.start()
 
@@ -60,7 +62,8 @@ class WindowEventCommand(sublime_plugin.EventListener):
         if repl_process:
             repl_process.stdin.write("(quit)\n")
             repl_process.terminate()
-            repl_process = NoneF
+            repl_process = None
+
 
 class ReplQuitCommand(sublime_plugin.TextCommand):
 
@@ -72,13 +75,14 @@ class ReplQuitCommand(sublime_plugin.TextCommand):
             repl_process.stdin.write("(quit)\n")
             repl_process.terminate()
             repl_process = None
-            output_view.insert(edit, output_view.size(), "AML process terminated. Bye :-)")
+            output_view.insert(
+                edit, output_view.size(), "AML process terminated. Bye :-)")
 
 
 class ReplEvalCommand(sublime_plugin.TextCommand):
 
     def last_sexp(self, string):
-        sexp, bracet_count, bracket_match, done  = "", 0, 0, 0
+        sexp, bracet_count, bracket_match, done = "", 0, 0, 0
 
         for c in reversed(string):
             if c == ')':
@@ -89,12 +93,12 @@ class ReplEvalCommand(sublime_plugin.TextCommand):
                 bracket_match -= 1
                 bracet_count += 1
 
-            if done == 0 and bracet_count > 0 :
+            if done == 0 and bracet_count > 0:
                 sexp = c + sexp
-            
+
             elif done == 1 and c == '\'':
                 sexp = c + sexp
-            
+
             elif done > 1:
                 break
 
@@ -111,16 +115,20 @@ class ReplEvalCommand(sublime_plugin.TextCommand):
             position = self.view.sel()[0]
 
             if position.begin() == position.end():
-                input_substr = self.last_sexp(self.view.substr(sublime.Region(0, self.view.size())))
+                input_substr = self.last_sexp(
+                    self.view.substr(sublime.Region(0, self.view.size())))
             else:
-                input_substr = self.view.substr(sublime.Region(position.begin(), position.end()))  
+                input_substr = self.view.substr(
+                    sublime.Region(position.begin(), position.end()))
 
             output_view.insert(edit, self.view.size(), "\n")
             repl_process.stdin.write("%s\n" % input_substr)
-  
+
             output_view.run_command('output_lines')
         else:
-            output_view.insert(edit, output_view.size(), "No AML process initialized. Please restart AMLRepl.\n")
+            output_view.insert(
+                edit, output_view.size(), "No AML process initialized. Please restart AMLRepl.\n")
+
 
 class FileEvalCommand(sublime_plugin.TextCommand):
 
@@ -129,11 +137,14 @@ class FileEvalCommand(sublime_plugin.TextCommand):
         global output_view
 
         if repl_process:
-            input_substr = self.view.substr(sublime.Region(0, self.view.size()))
+            input_substr = self.view.substr(
+                sublime.Region(0, self.view.size()))
             repl_process.stdin.write("%s\n" % input_substr)
             output_view.run_command('output_lines')
         else:
-            output_view.insert(edit, self.view.size(), "No AML process initialized. Please restart AMLRepl.\n")
+            output_view.insert(
+                edit, self.view.size(), "No AML process initialized. Please restart AMLRepl.\n")
+
 
 class OutputLinesCommand(sublime_plugin.TextCommand):
 
@@ -151,38 +162,50 @@ class OutputLinesCommand(sublime_plugin.TextCommand):
         self.view.run_command("goto_line", {"line": self.view.size()})
         output_lines = []
 
+
 class AmlReferenceManualCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         global aml_manual_path
-        
+
         url = "file:///" + aml_manual_path + "index.html"
         webbrowser.open_new(url)
 
+
 class AmlGuiCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         if repl_process:
             output_view.insert(edit, self.view.size(), "\n")
             repl_process.stdin.write("%s\n" % "(aml)")
-  
+
             output_view.run_command('output_lines')
         else:
-            output_view.insert(edit, output_view.size(), "No AML process initialized. Please restart AMLRepl.\n")
+            output_view.insert(
+                edit, output_view.size(), "No AML process initialized. Please restart AMLRepl.\n")
+
 
 class AunitGuiCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         if repl_process:
             output_view.insert(edit, self.view.size(), "\n")
 
-            repl_process.stdin.write("%s\n" % "(compile-system :aunit-core-system)") 
+            repl_process.stdin.write(
+                "%s\n" % "(compile-system :aunit-core-system)")
             output_view.run_command('output_lines')
-            repl_process.stdin.write("%s\n" % "(compile-system :aunit-print-system)") 
+            repl_process.stdin.write(
+                "%s\n" % "(compile-system :aunit-print-system)")
             output_view.run_command('output_lines')
-            repl_process.stdin.write("%s\n" % "(compile-system :aunit-gui-system)") 
+            repl_process.stdin.write(
+                "%s\n" % "(compile-system :aunit-gui-system)")
             output_view.run_command('output_lines')
-            repl_process.stdin.write("%s\n" % "(compile-system :aunit-main-system)") 
+            repl_process.stdin.write(
+                "%s\n" % "(compile-system :aunit-main-system)")
             output_view.run_command('output_lines')
-            
-            repl_process.stdin.write("%s\n" % "(aunit)") 
+
+            repl_process.stdin.write("%s\n" % "(aunit)")
             output_view.run_command('output_lines')
         else:
-            output_view.insert(edit, output_view.size(), "No AML process initialized. Please restart AMLRepl.\n")
+            output_view.insert(
+                edit, output_view.size(), "No AML process initialized. Please restart AMLRepl.\n")
